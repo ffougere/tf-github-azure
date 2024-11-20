150be4d65363b769f68849f0ef0007a5374b26da
unknown <fabrice.fougere@gmail.com>
2024-11-20 18:10:42 -0500

    terraform code update

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
-resource "azurerm_sql_server" "example" {
-  name                = "mySqlServer"
-  resource_group_name = "myResourceGroup"
-  location            = "West US"
-  version             = "12.0"
+provider "azurerm" {
+  features {}
+  #skip_provider_registration = true
+  resource_provider_registrations = "none"
+  subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"
 }
 
-resource "azurerm_sql_database" "example" {
-  name                = "mydb"
-  resource_group_name = "myResourceGroup"
-  server_name        = azurerm_sql_server.example.name
+resource "azurerm_mssql_server" "example" {
+  name                = "ffomysqlserver"
+  resource_group_name = "kml_rg_main-8968de8c223043d8"
+  location            = "West US"
+  version             = "12.0"
+  administrator_login          = "ffo"
+  administrator_login_password = "Temporaire78220*"
+}
+
+resource "azurerm_mssql_database" "example" {
+  name                = "mydbazuresql"
+  #resource_group_name = "kml_rg_main-8968de8c223043d8"
+  #server_name        = azurerm_mssql_server.example.name
+  server_id           = azurerm_mssql_server.example.id
 }
