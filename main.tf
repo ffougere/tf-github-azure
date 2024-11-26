cc29c16eb379d752eca8a90a97fcdf761f160c75
unknown <fabrice.fougere@gmail.com>
2024-11-26 15:07:30 -0500

    Updated .gitignore to ignore .terraform/

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "azurerm" {
   features {}
   #skip_provider_registration = true
   resource_provider_registrations = "none"
-  subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_mssql_server" "example" {
   name                = "ffomysqlserver"
-  resource_group_name = "kml_rg_main-8968de8c223043d8"
+  resource_group_name = var.rg_id
   location            = "West US"
   version             = "12.0"
   administrator_login          = "ffo"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
 # Valeurs des variables
 subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"
 client_id       = "your_client_id"
+rg_id           = "kml_rg_main-5dbfca87a76b4e47"
