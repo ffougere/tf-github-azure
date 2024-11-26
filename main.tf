c413795a229f099e5300a4ce62824cd068fd01c9
unknown <fabrice.fougere@gmail.com>
2024-11-26 15:23:08 -0500

    Merge unrelated histories

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 provider "azurerm" {
   features {}
   #skip_provider_registration = true
@@ -17,20 +16,5 @@ resource "azurerm_mssql_server" "example" {
 
 resource "azurerm_mssql_database" "example" {
   name                = "mydbazuresql"
-  #resource_group_name = "kml_rg_main-8968de8c223043d8"
-  #server_name        = azurerm_mssql_server.example.name
   server_id           = azurerm_mssql_server.example.id
-=======
-resource "azurerm_sql_server" "example" {
-  name                = "mySqlServer"
-  resource_group_name = "myResourceGroup"
-  location            = "West US"
-  version             = "12.0"
-}
-
-resource "azurerm_sql_database" "example" {
-  name                = "mydb"
-  resource_group_name = "myResourceGroup"
-  server_name        = azurerm_sql_server.example.name
->>>>>>> bb9720bcedf31a47560d3df71a0d006b8ae01999
 }
